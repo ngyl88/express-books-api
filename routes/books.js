10bb6bdc97b0a6f0dcfb8ee0a018cc453db97ff4
ngyl88 <ng_yl88@hotmail.com>
2018-07-10 17:01:39 +0800

    CRUD with express and mongoDB

--- routes/books.js
+++ routes/books.js
@@ -1,25 +1,50 @@
 const express = require("express");
 const router = express.Router();
 
+// MODEL
+const Book = require("../models/book");
+
 /* GET books listing. */
-router.get("/", (req, res, next) => {
-  res.json({ message: "respond with all books" });
+router.get("/", async (req, res, next) => {
+  const books = await Book.find();
+  res.json(books);
 });
 
-router.get("/:id", (req, res, next) => {
-  res.json({ message: `get book with id ${req.params.id}` });
+router.get("/:id", async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+  book === null ? next() : res.json(book);
 });
 
-router.post("/", (req, res, next) => {
-  res.json({ message: `create new book using data from ${req.body}` });
+router.post("/", async (req, res, next) => {
+  const newBook = new Book({
+    title: req.body.title,
+    author: req.body.author
+  });
+
+  const result = await newBook.save();
+
+  res
+    .status(201)
+    .json({ message: `created new book successfully with id ${result._id}` });
 });
 
-router.put("/:id", (req, res, next) => {
-  res.json({ message: `update book with id ${req.params.id}` });
+router.put("/:id", async (req, res, next) => {
+  const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body);
+  updatedBook === null
+    ? next()
+    : res.json({ message: `updated book with id ${req.params.id}` });
 });
 
-router.delete("/:id", (req, res, next) => {
-  res.json({ message: `delete book with id ${req.params.id}` });
+router.delete("/:id", async (req, res, next) => {
+  const bookToDelete = await Book.findByIdAndDelete(req.params.id);
+  bookToDelete === null
+    ? next()
+    : res.json({ message: `delete book with id ${req.params.id}` });
+});
+
+router.use((req, res) => {
+  // console.log('In 404 handler:', req.params, req.path);
+  res.status(404).json({ message: `Book with id ${req.path} Not Found!` });
 });
 
 module.exports = router;
